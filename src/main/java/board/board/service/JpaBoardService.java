eddaeb7bc2602c91f5a2ca77fadb3a5636584ded
LaVi <lastvirgo266@gmail.com>
2020-02-12 02:09:31 +0900

    JPA Exmaple

--- src/main/java/board/board/service/JpaBoardService.java
+++ src/main/java/board/board/service/JpaBoardService.java
@@ -1,5 +1,19 @@
 package board.board.service;
 
-public class JpaBoardService {
+import java.util.List;
 
+
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import board.board.entity.BoardEntity;
+import board.board.entity.BoardFileEntity;
+
+public interface JpaBoardService {
+	
+	List<BoardEntity> selectBoardList() throws Exception;
+	
+	void saveBoard(BoardEntity board, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception;
+	BoardEntity selectBoardDetail(int boardIndx) throws Exception;
+	void deletBoard(int boardIdx);
+	BoardFileEntity selectBoardFileInformation(int boardIdx, int idx) throws Exception;
 }
